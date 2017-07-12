a96b37822539a8689b360f07fcb0b925b60f7bfb
root <root@smu2017.pxmongoajoiu3oswxyf1na5nmc.bx.internal.cloudapp.net>
2017-07-12 03:32:20 +0000

    added secrets.py serverside

---
+++ database/test.py
@@ -0,0 +1,5 @@
+from database_handler import DatabaseHandler as DH
+
+dh = DH("SMU-logs.db")
+
+print(dh.start_work("zeynepnazakinci@gmail.com", '"Learn to Code Awesome Websites" by General Assembly'))
