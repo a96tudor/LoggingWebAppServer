44a0e01a698145e123f668bd5178c23b0881c1f8
root <root@smu2017.pxmongoajoiu3oswxyf1na5nmc.bx.internal.cloudapp.net>
2017-07-13 08:01:43 +0000

    added small change

--- server.py
+++ server.py
@@ -290,6 +290,7 @@ def logout():
         data = request.json
         if "id" in data and isinstance(data["id"], str):
             result = dh.logout_user(data["id"])
+            print(result)
             return jsonify(result)
         else:
             return Response(status=400, response="Wrong request format")
