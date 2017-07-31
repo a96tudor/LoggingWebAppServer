59ee4afe606ade217d87bad04cd93f7ecd07ec1d
root <root@smu2017.pxmongoajoiu3oswxyf1na5nmc.bx.internal.cloudapp.net>
2017-07-31 04:20:36 +0000

    added June/July raw archive

--- database/exceptions.py
+++
@@ -1,7 +0,0 @@
-class Exception:
-
-    def __init__(self, message):
-        self.error_message = message
-
-    def __str__(self):
-        return str(self.error_message)
--- database/test-migrator.py
+++
@@ -1,6 +0,0 @@
-from database_handler import DatabaseHandler as DH
-
-dh = DH("SMU-logs.db")
-
-print(dh.get_logs())
-print(dh.get_working_users())
